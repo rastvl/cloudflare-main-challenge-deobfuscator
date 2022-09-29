149eb976a54d439c199ce7c6cdcc291dbf616873
Vladislav Rastoropov <rastmath23@gmail.com>
2022-09-29 18:57:58 +0300

    README update

--- index.js
+++ index.js
@@ -8,8 +8,4 @@ const sourceCode = fs.readFileSync("input/cf-main-challenge.js", {
 const deobfuscator = new Deobfuscator(sourceCode);
 const code = deobfuscator.deobfuscate();
 
-// const t = require('@babel/types');
-// const generate = require('@babel/generator').default;
-// console.log(JSON.parse(generate(t.arrayExpression([t.numericLiteral(1), t.numericLiteral(2)])).code));
-
 fs.writeFile("./output/deobfuscated_code.js", code, () => console.log("done!"));
